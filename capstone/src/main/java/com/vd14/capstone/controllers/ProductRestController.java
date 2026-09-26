8a4300b462305ed38335285d9ebac008b67e3ac0
valdal14 <valdal14@gmail.com>
2026-09-26 17:12:23 +0200

    add ProductRestController and verified the endpoint returns the correct stored product by its id.

---
+++ capstone/src/main/java/com/vd14/capstone/controllers/ProductRestController.java
@@ -0,0 +1,24 @@
+package com.vd14.capstone.controllers;
+
+import com.vd14.capstone.entities.Product;
+import com.vd14.capstone.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("product")
+public class ProductRestController {
+    @Autowired
+    private final ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Optional<Product> getProduct(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+}
---
+++ capstone/src/test/java/com/vd14/capstone/controllers/ProductRestControllerTest.java
@@ -0,0 +1,44 @@
+package com.vd14.capstone.controllers;
+
+import com.vd14.capstone.entities.Product;
+import com.vd14.capstone.services.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductRestController.class)
+class ProductRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService productService;
+
+    @Test
+    void getProductById() throws Exception {
+        // ARRANGE
+        Optional<Product> product = Optional.of(new Product(1L, "Pixel Watch 5", BigDecimal.valueOf(499.00)));
+        when(productService.findById(1L)).thenReturn(product);
+        // ACT
+        mockMvc.perform(get("/product/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Pixel Watch 5"))
+                .andExpect(jsonPath("$.price").value(499.00));
+        // VERIFY
+        verify(productService).findById(1L);
+    }
+}
